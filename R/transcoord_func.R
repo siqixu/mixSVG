da5399cf73a39e669237e9b5b3281bfd45e088cf
siqixu <72850541+siqixu@users.noreply.github.com>
2024-09-19 11:25:06 +0800

    Update transcoord_func.R

--- R/transcoord_func.R
+++ R/transcoord_func.R
@@ -2,12 +2,12 @@ transcoord_func = function (si, transfunc = "gaussian", q = 0.2, c = 0)
 {
   si <- scale(si)
   l <- quantile(abs(si), probs = q)
-  si = si + c
+
   if (transfunc == "gaussian") {
-    out <- exp(-si^2/(2 * l^2))
+    out <- exp(-(si + c)^2/(2 * l^2))
   }
   if (transfunc == "cosine") {
-    out <- cos(2 * pi * si/l)
+    out <- cos(2 * pi * si/l + pi*c/2)
   }
   return(out)
 }
