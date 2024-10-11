adf3a8149b4a8ff46b8dbdc7216cf1652de4aac9
siqixu <72850541+siqixu@users.noreply.github.com>
2024-10-11 16:19:59 +0800

    Update transcoord_func.R

--- R/transcoord_func.R
+++ R/transcoord_func.R
@@ -2,7 +2,7 @@ transcoord_func = function (si, transfunc = "gaussian", l = 0.2, c = 0)
 {
   si <- scale(si)
   if (transfunc == "gaussian") {
-    out <- exp(-(si + c)^2/(2*l))
+    out <- exp(-(si + c)^2/(2*l^2))
   }
   if (transfunc == "cosine") {
     out <- cos(pi*si/l + pi*c/2)
